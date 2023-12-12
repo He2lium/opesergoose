19539e86e4711f371f0cce1ff59d10881d8200e4
pavelprna <pavel@prna.ru>
2023-12-12 10:49:59 +0700

    fix: null doc

--- src/middlewares/postUpdate.ts
+++ src/middlewares/postUpdate.ts
@@ -1,14 +1,20 @@
 import { omitDoc } from '../utils/omitDoc'
 import { PostQueryMiddleware } from '../types/middlewares'
 
-export const postUpdate: PostQueryMiddleware = (openSearchClient, index, populates = [], forbiddenFields = []) =>
+export const postUpdate: PostQueryMiddleware = (
+  openSearchClient,
+  index,
+  populates = [],
+  forbiddenFields = [],
+) =>
   async function (doc, next) {
-    await openSearchClient.index({
-      index,
-      id: doc.id,
-      body: omitDoc(await doc.populate(populates), forbiddenFields),
-      refresh: true,
-    })
+    if (doc)
+      await openSearchClient.index({
+        index,
+        id: doc.id,
+        body: omitDoc(await doc.populate(populates), forbiddenFields),
+        refresh: true,
+      })
 
     next()
   }
